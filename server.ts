c390e180fd2632d355850a41d331a0ee240d17a9
Ajordon11 <andrej.fula@gmail.com>
2024-11-30 21:52:58 +0100

    Allow remote client server to connect to socket

--- server.ts
+++ server.ts
@@ -3,10 +3,16 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+let dotenv = require('dotenv').config();
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173", dotenv.parsed.FRONTEND_URL],
+    credentials: true
+  }
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -15,7 +21,7 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('a user connected ', socket.id);
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
