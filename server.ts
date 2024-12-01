02ae9e6a671ea288673e5db20a78d1f92448b2aa
Ajordon11 <andrej.fula@gmail.com>
2024-12-01 09:03:08 +0100

    Some small changes

--- server.ts
+++ server.ts
@@ -1,38 +1,42 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-import { Server } from 'socket.io';
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [process.env.CLIENT_REMOTE_URL, process.env.CLIENT_LOCAL_URL],
-    methods: ['GET', 'POST'],
-    credentials: true
-  }
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected ', socket.id);
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-    socket.on('chat message', (msg: string) => {
-        console.log('message: ' + msg);
-        io.emit('server-info', {
-            message: msg
-        });
+io.on("connection", (socket) => {
+  console.log("a user connected ", socket.id);
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("chat message", (msg: string) => {
+    console.log("message: " + msg);
+    io.emit("server-info", {
+      message: msg,
     });
   });
 
+  socket.on("message", (message) => {
+    console.log("message? ", message);
+  });
+});
+
 server.listen(process.env.PORT, () => {
- console.log('Server is running on port ' + process.env.PORT);
-})
+  console.log("Server is running on port " + process.env.PORT);
+});
